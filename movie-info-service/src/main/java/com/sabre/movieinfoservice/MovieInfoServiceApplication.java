449ab6b0a59fa0b9b2e1653444d1af86bac2b1d7
janswain <janmejay.swain@sabre.com>
2021-01-25 12:14:44 +0530

    Commited the changes

---
+++ movie-info-service/src/main/java/com/sabre/movieinfoservice/MovieInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sabre.movieinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServiceApplication.class, args);
+	}
+
+}
---
+++ movie-info-service/src/main/java/com/sabre/movieinfoservice/resources/SpringBootApplicationExample.java
@@ -0,0 +1,17 @@
+package com.sabre.movieinfoservice.resources;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootApplicationExample {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to hello world............";
+
+	}
+
+}
