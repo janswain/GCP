d5e98b58e8b560a66f8562735502d9b661575fa9
janswain <janmejay.swain@sabre.com>
2021-01-26 12:27:46 +0530

    NgCorp

---
+++ ngcorp-service/src/main/java/com/sabre/ngcorp/resources/SpringBootApplicationExample.java
@@ -0,0 +1,17 @@
+package com.sabre.ngcorp.resources;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootApplicationExample {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to GetThere..........";
+
+	}
+
+}
