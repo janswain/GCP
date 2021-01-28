0289562266330391b1fab2d642bd9438d29d27bf
janswain <janmejay.swain@sabre.com>
2021-01-28 18:57:05 +0530

    Git example

---
+++ getthere-git-gcp-sample/src/main/java/com/sabre/getthere/HelloAppEngine.java
@@ -0,0 +1,26 @@
+package com.sabre.getthere;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "HelloAppEngine",
+    urlPatterns = {"/gt/git"}
+)
+public class HelloAppEngine extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().print("Project downloaded from GIT... !\r\n");
+
+  }
+}
